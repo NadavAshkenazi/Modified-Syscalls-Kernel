00cf0bd466e1a4f0895ba621b394413df4ea16fa
Nadav Ashkenazi <nadavash@campus.technion.ac.il>
2020-12-02 15:49:54 +0200

    init commit

---
+++ cmake-build-debug/CMakeFiles/os_hw2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/os_hw2.dir/main.cpp.o"
+  "os_hw2"
+  "os_hw2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/os_hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
